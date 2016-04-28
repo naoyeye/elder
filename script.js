c49785cb10325774ad95e3f6a39c315585322fc6
hanjiyun <naoyeye@gmail.com>
2016-04-28 22:49:51 +0800

    fix wechat

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ $(function() {
         e.preventDefault();
         $(this).find('audio')[0].play();
     });
+    
+    var ua = navigator.userAgent.toLowerCase();
 
+    var iswechat = !!ua.match(/micromessenger/);;
+
+    if (!iswechat) {
+        $('#tips').show();
+    }
 })
